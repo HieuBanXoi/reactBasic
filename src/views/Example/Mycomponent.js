a9c1b632ba4fde197296dc02022ad63899951012
HieuBanXoi <bom44380@gmail.com>
2024-10-15 21:14:32 +0700

    State basic

---
+++ src/views/Example/Mycomponent.js
@@ -0,0 +1,28 @@
+import React from "react";
+class MyComponent extends React.Component {
+  state = {
+    name: "HieuBanXoi",
+    channel: "abcxyz",
+  };
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <>
+        <div className="first">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          My name is {this.state["name"]}
+        </div>
+        <div className="second">My youtube channel : {this.state.channel}</div>
+      </>
+    );
+  }
+}
+export default MyComponent;
