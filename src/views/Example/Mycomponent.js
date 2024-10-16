7333e134e7d4e60fc74c6cc8297c9eba0fbc8932
HieuBanXoi <bom44380@gmail.com>
2024-10-16 20:46:42 +0700

    Form

--- src/views/Example/Mycomponent.js
+++ src/views/Example/Mycomponent.js
@@ -1,26 +1,51 @@
 import React from "react";
 class MyComponent extends React.Component {
   state = {
-    name: "HieuBanXoi",
-    channel: "abcxyz",
+    firstName: "",
+    lastName: "",
   };
-  handleOnChangeName = (event) => {
+
+  handleChangeFirstName = (event) => {
     this.setState({
-      name: event.target.value,
+      firstName: event.target.value,
     });
   };
+  handleChangeLastName = (event) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("hieu");
+  };
   render() {
+    console.log(this.state);
     return (
       <>
-        <div className="first">
+        <form>
+          <label htmlFor="fname">First name</label>
+          <br></br>
           <input
-            value={this.state.name}
             type="text"
-            onChange={(event) => this.handleOnChangeName(event)}
-          />
-          My name is {this.state["name"]}
-        </div>
-        <div className="second">My youtube channel : {this.state.channel}</div>
+            value={this.state.firstName}
+            onChange={(event) => this.handleChangeFirstName(event)}
+          ></input>
+          <br></br>
+          <label htmlFor="lname">Last name</label>
+          <br></br>
+          <input
+            type="text"
+            value={this.state.lastName}
+            onChange={(event) => this.handleChangeLastName(event)}
+          ></input>
+          <br></br>
+          <input
+            type="submit"
+            value="Submit"
+            onClick={(event) => this.handleSubmit(event)}
+          ></input>
+        </form>
       </>
     );
   }
